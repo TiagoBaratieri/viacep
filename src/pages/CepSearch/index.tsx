b23d7e7746eff897f548fdc0148b07f40884d6a0
Tiago Baratieri <tiagobaratieri97@gmail.com>
2021-08-27 17:10:51 -0300

    ResultCard,useState, axios

--- src/pages/CepSearch/index.tsx
+++ src/pages/CepSearch/index.tsx
@@ -1,25 +1,79 @@
+import axios from 'axios';
+import { useState } from 'react';
+import ResultCard from '../../components/ResultCard';
 import './styles.css';
+
+type FormData = {
+  cep: string;
+};
+
+type Address = {
+  logradouro: string;
+  localidade: string;
+  bairro: string;
+  uf: string;
+  ddd: string;
+};
+
 const CepSearch = () => {
-    return (
-      <div className="cep-search-container">
-        <h1 className="text-primary">Busca CEP</h1>
-        <div className="container search-container">
-          <form>
-            <div className="form-container">
-              <input
-                type="text"
-                className="search-input"
-                placeholder="CEP (somente números)"
-                onChange={() => {}}
-              />
-              <button type="submit" className="btn btn-primary search-button">
-                Buscar
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
-    );
+  const [address, setAddress] = useState<Address>();
+  const [formData, setFormdata] = useState<FormData>({
+    cep: ''
+  });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setFormdata({ ...formData, [name]: value });
   };
-  
-  export default CepSearch;
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    axios
+      .get(`https://viacep.com.br/ws/${formData.cep}/json/`)
+      .then((response) => {
+        setAddress(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        setAddress(undefined);
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="cep-search-container">
+      <h1 className="text-primary">Busca CEP</h1>
+      <div className="container search-container">
+        <form onSubmit={handleSubmit}>
+          <div className="form-container">
+            <input
+              type="text"
+              name="cep"
+              value={formData.cep}
+              className="search-input"
+              placeholder="CEP (somente números)"
+              onChange={handleChange}
+            />
+            <button type="submit" className="btn btn-primary search-button">
+              Buscar
+            </button>
+          </div>
+        </form>
+        {address && (
+          <>
+            <ResultCard title="Logradouro" description={address.logradouro} />
+            <ResultCard title="Localidade" description={address.localidade} />
+            <ResultCard title="Bairro" description={address.bairro} />
+            <ResultCard title="uf" description={address.uf} />
+            <ResultCard title="ddd" description={address.ddd} />
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CepSearch;
